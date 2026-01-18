50cfe78c1a0d8188e9c5e0ac82bc9d8a13446fc5
Diego Pereyra <23diegopereyra@gmail.com>
2026-01-18 10:30:39 -0800

    furniture class started

---
+++ Assets/Scripts/Furniture.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Furniture : MonoBehaviour
+{
+    public string furnitureName;
+
+    // placing furniture on the xz-plane
+    public void SetPosition(Vector2 position)
+    {
+        this.transform.position = new Vector3(position.x, this.transform.position.y, position.y);
+        // TODO snap y-position based on objects caught in downward raycast
+        // ^ grid system subtask, i believe
+    }
+
+    // rotating furniture on the y-axis
+    public void SetRotation(float rotation)
+    {
+        this.transform.eulerAngles = new Vector3(transform.eulerAngles.x, rotation, transform.eulerAngles.z);
+    }
+
+    void Update()
+    {
+        // TESTING SETPOSITION & SETROTATION, TODO: REMOVE AFTER PLAYER INPUTS IMPLEMENTED -----------------------------
+        // -- testing SetPosition
+        if (Input.GetKeyDown(KeyCode.A))
+            SetPosition(new Vector2(transform.position.x - 1f, transform.position.z));
+        if (Input.GetKeyDown(KeyCode.D))
+            SetPosition(new Vector2(transform.position.x + 1f, transform.position.z));
+        if (Input.GetKeyDown(KeyCode.W))
+            SetPosition(new Vector2(transform.position.x, transform.position.z + 1f));
+        if (Input.GetKeyDown(KeyCode.S))
+            SetPosition(new Vector2(transform.position.x, transform.position.z - 1f));
+        // -- testing SetRotation
+        if (Input.GetKeyDown(KeyCode.R))
+            SetRotation(transform.eulerAngles.y + 90f);
+        // -------------------------------------------------------------------------------------------------------------
+    }
+}
