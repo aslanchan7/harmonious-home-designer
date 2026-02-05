725d9fe2cfea53b9f9f1a42a36285cebf2e1493e
Nhat Tran <nhatt.dev@gmail.com>
2026-02-04 19:33:27 -0800

    fix rotate snap for odd-by-odd or even-by-even sized furnitures

--- Assets/Scripts/Controllers/PlayerControls.cs
+++ Assets/Scripts/Controllers/PlayerControls.cs
@@ -1,26 +1,39 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Collections;
 
 public class PlayerControls : MonoBehaviour
 {
-    [HideInInspector] public Vector3 MousePos;
+    [HideInInspector]
+    public Vector3 MousePos;
     private Furniture selectedFurniture;
 
     [Header("Actions")]
     private InputAction mousePositionAction;
-    [SerializeField] private InputActionReference clickAction;
-    [SerializeField] private InputActionReference rotateAction;
+
+    [SerializeField]
+    private InputActionReference clickAction;
+
+    [SerializeField]
+    private InputActionReference rotateAction;
 
     [Header("References")]
-    [SerializeField] private MouseIndicator mouseIndicator;
+    [SerializeField]
+    private MouseIndicator mouseIndicator;
 
-    private readonly Vector2[] rotationSnapOffset =
+    private readonly Vector2[] evenSizeSnapOffset =
+    {
+        Vector2.down,
+        Vector2.right,
+        Vector2.up,
+        Vector2.left,
+    };
+    private readonly Vector2[] oddSizeSnapOffset =
     {
         new Vector2(-0.5f, -0.5f),
         new Vector2(-0.5f, 0.5f),
         new Vector2(0.5f, 0.5f),
-        new Vector2(0.5f, -0.5f)
+        new Vector2(0.5f, -0.5f),
     };
     private int rotateSnapOffsetIndex;
 
@@ -56,7 +69,8 @@ public class PlayerControls : MonoBehaviour
         {
             if (hit.collider.CompareTag("Furniture") && hit.collider != null)
             {
-                selectedFurniture = hit.collider.transform.parent.GetComponent<Furniture>();
+                selectedFurniture =
+                    hit.collider.transform.parent.GetComponent<Furniture>();
                 rotateSnapOffsetIndex = 0;
                 GridSystem.Instance.ShowGridVisualizer();
                 StartCoroutine(DragUpdate());
@@ -84,7 +98,7 @@ public class PlayerControls : MonoBehaviour
         selectedFurniture.TryPlace();
         selectedFurniture.SetNormalMat();
         selectedFurniture.SetColliderEnabled(true);
-        mouseIndicator.Size = new (1, 1);
+        mouseIndicator.Size = new(1, 1);
     }
 
     private void OnRotate(InputAction.CallbackContext callbackContext)
@@ -99,26 +113,37 @@ public class PlayerControls : MonoBehaviour
             mouseIndicator.Rotate();
             return;
         }
-        
-        if ((selectedFurniture.Size.x + selectedFurniture.Size.y) % 2 == 0)
+
+        if (selectedFurniture.Size.x == selectedFurniture.Size.y)
         {
             mouseIndicator.Rotate();
             selectedFurniture.SetLocationAsValid();
             return;
         }
 
-        Vector2 currentPosition = selectedFurniture.LastValidPosition;
-        bool rotateSuccess = false;
+        bool sizeIsEven =
+            (selectedFurniture.Size.x + selectedFurniture.Size.y) % 2 == 0;
         selectedFurniture.SetColliderEnabled(false);
+        if (sizeIsEven && selectedFurniture.CheckValidPos())
+        {
+            selectedFurniture.SetColliderEnabled(true);
+            mouseIndicator.Rotate();
+            selectedFurniture.SetLocationAsValid();
+            return;
+        }
+
+        Vector2 currentPosition = selectedFurniture.LastValidPosition;
+        Vector2[] offsetsToCheck = sizeIsEven
+            ? evenSizeSnapOffset
+            : oddSizeSnapOffset;
         for (int i = 0; i < 4; i++)
         {
-            Vector2 testPosition = currentPosition + rotationSnapOffset[
-                (rotateSnapOffsetIndex + i) % 4
-            ];
+            Vector2 testPosition =
+                currentPosition
+                + offsetsToCheck[(rotateSnapOffsetIndex + i) % 4];
             selectedFurniture.DisplayPosition = testPosition;
             if (selectedFurniture.CheckValidPos())
             {
-                rotateSuccess = true;
                 // Make sure that if the furniture is rotated consecutively,
                 // it would favor the offset direction opposite to the
                 // current offset
@@ -126,15 +151,12 @@ public class PlayerControls : MonoBehaviour
                 mouseIndicator.Rotate();
                 selectedFurniture.SetColliderEnabled(true);
                 selectedFurniture.SetLocationAsValid();
-                break;
+                return;
             }
         }
 
-        if (!rotateSuccess)
-        {
-            selectedFurniture.SetColliderEnabled(true);
-            selectedFurniture.ResetToValidLocation();
-        }
+        selectedFurniture.SetColliderEnabled(true);
+        selectedFurniture.ResetToValidLocation();
     }
 
     public void RaycastMouse()
@@ -142,14 +164,17 @@ public class PlayerControls : MonoBehaviour
         Vector2 mousePos = mousePositionAction.ReadValue<Vector2>();
         Ray ray = Camera.main.ScreenPointToRay(mousePos);
         RaycastHit hit;
-        if (Physics.Raycast(
-            ray,
-            out hit,
-            float.PositiveInfinity,
-            GridSystem.Instance.PlacementLayer
-        ))
+        if (
+            Physics.Raycast(
+                ray,
+                out hit,
+                float.PositiveInfinity,
+                GridSystem.Instance.PlacementLayer
+            )
+        )
         {
             MousePos = hit.point;
         }
     }
 }
+
