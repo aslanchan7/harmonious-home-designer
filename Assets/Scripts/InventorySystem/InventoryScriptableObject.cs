b60939689ee6ef65871edf9ab8fc4021ea8685d7
aslanchan7 <aslanchan7@gmail.com>
2026-02-20 17:14:21 -0800

    created icon editor tool

--- Assets/Scripts/InventorySystem/InventoryScriptableObject.cs
+++ Assets/Scripts/InventorySystem/InventoryScriptableObject.cs
@@ -10,7 +10,9 @@ public class InventoryScriptableObject : ScriptableObject
 [System.Serializable]
 public class InventoryItem
 {
+    public string Name;
     public GameObject Prefab;
+    public Texture2D Icon;
     public int MaxPlacements;
     public int CurrentPlacedCount = 0;
 }
